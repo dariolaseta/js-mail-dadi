7fe52d96a41aa5a211efcd4b02a33d5f46b10010
dariolaseta <dario.laseta@gmail.com>
2023-05-17 13:48:05 +0200

    added winner to document

--- js/myscript.js
+++ js/myscript.js
@@ -1,14 +1,24 @@
+// Dice game
 let playerNumber = Math.floor(Math.random() * 6) + 1;
 let comNumber = Math.floor(Math.random() * 6) + 1;
+let winnerText;
 
 if(playerNumber > comNumber){
     console.log("Ha vinto il giocatore!");
     console.log(playerNumber + " Giocatore", comNumber + " Computer");
+    winnerText = "Ha vinto il giocatore!";
 }else if(playerNumber == comNumber){
     console.log("Pareggio!");
     console.log(playerNumber + " Giocatore", comNumber + " Computer");
+    winnerText = "Pareggio!";
 }
 else{
     console.log("Ha vinto il computer!");
     console.log(playerNumber + " Giocatore", comNumber + " Computer");
+    winnerText = "Ha vinto il computer!";
 }
+
+document.getElementById("output-dice").innerHTML = winnerText; 
+
+// Mail
+
