e6cc4daf62f0db5d3f4d90a7afd5d40c8aa44609
dariolaseta <dario.laseta@gmail.com>
2023-05-17 13:41:21 +0200

    created dice game script

--- js/myscript.js
+++ js/myscript.js
@@ -1,6 +1,14 @@
-let randomNumber = Math.floor(Math.random() * 6) + 1;
-console.log(randomNumber);
+let playerNumber = Math.floor(Math.random() * 6) + 1;
+let comNumber = Math.floor(Math.random() * 6) + 1;
 
-
-let playerNumber;
-let comNumber;
+if(playerNumber > comNumber){
+    console.log("Ha vinto il giocatore!");
+    console.log(playerNumber + " Giocatore", comNumber + " Computer");
+}else if(playerNumber == comNumber){
+    console.log("Pareggio!");
+    console.log(playerNumber + " Giocatore", comNumber + " Computer");
+}
+else{
+    console.log("Ha vinto il computer!");
+    console.log(playerNumber + " Giocatore", comNumber + " Computer");
+}
